ded3226c171b27d2ff9bf526a48a91da37d35c1c
Alex Erling <alex.erling@gmail.com>
2018-03-19 15:18:23 -0500

    Completed checkpoint and assignment 5

---
+++ lib/bloc_record/collection.rb
@@ -0,0 +1,39 @@
+module BlocRecord
+  class Collection < Array
+    # #5
+    def update_all(updates)
+      ids = self.map(&:id)
+      # #6
+      self.any? ? self.first.class.update(ids, updates) : false
+    end
+
+    def take(num=1)
+      loop_point = 0
+      while loop_point < num
+        puts self.sample
+        loop_point += 1
+      end
+    end
+
+    def where(params)
+      key = params.keys.first.to_s
+      value = params.values.first.to_s
+      self.each do |item|
+        if item.send(key) == value
+          puts item
+        end
+      end
+    end
+
+    def not(params)
+      key = params.keys.first.to_s
+      value = params.values.first.to_s
+      self.each do |item|
+        if item.send(key) != value
+          puts item
+        end
+      end
+    end
+
+  end
+end
--- lib/bloc_record/persistence.rb
+++ lib/bloc_record/persistence.rb
@@ -28,6 +28,16 @@ module Persistence
     true
   end
 
+  def update_attribute(attribute, value)
+    self.class.update(self.id, { attribute => value })
+  end
+
+
+   def update_attributes(updates)
+     self.class.update(self.id, updates)
+   end
+
+
   module ClassMethods
     def create(attrs)
       attrs = BlocRecord::Utility.convert_keys(attrs)
@@ -43,5 +53,43 @@ module Persistence
       data["id"] = connection.execute("SELECT last_insert_rowid();")[0][0]
       new(data)
     end
+
+    def update(ids, updates)
+      case updates
+      when Hash
+        updates = BlocRecord::Utility.convert_keys(updates)
+        updates.delete "id"
+        updates_array = updates.map { |key, value| "#{key}=#{BlocRecord::Utility.sql_strings(value)}" }
+        if ids.class == Fixnum
+          where_clause = "WHERE id = #{ids};"
+        elsif ids.class == Array
+          where_clause = ids.empty? ? ";" : "WHERE id IN (#{ids.join(",")});"
+        else
+          where_clause = ";"
+        end
+
+        connection.execute <<-SQL
+          UPDATE #{table}
+          SET #{updates_array * ","} #{where_clause}
+        SQL
+        true
+      when Array
+        updates.each_with_index do |data, index|
+          update(ids[index], data)
+        end
+      end
+    end
+
+    def method_missing(method, *args, &block)
+      if method == :update_name
+        update(self.id, name: args[0])
+      end
+    end
+
+    def update_all(updates)
+      update(nil, updates)
+    end
+
+
   end
 end
